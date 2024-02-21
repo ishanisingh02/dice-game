3ef90164941bd51c7694f9e5e5243fd9e504bf92
ishanisingh02 <148678329+ishanisingh02@users.noreply.github.com>
2024-02-21 21:59:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomNumber1=Math.ceil(Math.random()*6);
 var randomNumber2=Math.ceil(Math.random()*6);
-var x1="./images/dice"+randomNumber1+".png";
-var x2="./images/dice"+randomNumber2+".png";
+var x1="./dice"+randomNumber1+".png";
+var x2="./dice"+randomNumber2+".png";
 document.querySelector(".img1").setAttribute("src",x1);
 document.querySelector(".img2").setAttribute("src",x2);
 if(randomNumber1>randomNumber2)
